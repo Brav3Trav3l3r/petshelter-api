0bd04d2de032cee7f631248e56e1eafdabd441fa
Brav3Trav3l3r <yashgajbhiye10@gmail.com>
2023-06-10 23:52:35 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import cors from 'cors'
+
+import petRoutes from './pets/routes/pets.routes.js'
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/pets', petRoutes)
+
+if(process.env.NODE_ENV !== 'test'){
+    app.listen(port, ()=> console.log(`[server]:  Server is runing on port ${port}`))
+}
+
+export default app
